f03458cde4ed8236366efa35a5310544c5e4389b
RISHAV DEV <85701241+dev26rishav@users.noreply.github.com>
2022-03-10 01:11:57 +0530

    Add initial files

---
+++ index.js
@@ -0,0 +1,81 @@
+function myfunction() { 
+
+    let bajao = this.innerHTML;
+
+    makeSound(bajao);
+    addAnimation(bajao);
+    
+}
+
+let number = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < number; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", myfunction);
+
+}
+
+document.addEventListener("keypress", function(event){
+    
+    let bajao = event.key;
+    makeSound(bajao);
+    addAnimation(bajao);
+
+});
+
+function makeSound(key){
+
+    switch (key) {
+
+        case "w":
+            let audio1 = new Audio('sounds/crash.mp3');
+            audio1.play();
+            break;
+        
+        case "a":
+            let audio2 = new Audio('sounds/kick-bass.mp3');
+            audio2.play();
+            break;
+
+        case "s":
+            let audio3 = new Audio('sounds/snare.mp3');
+            audio3.play();
+            break;
+
+        case "d":
+            let audio4 = new Audio('sounds/tom-1.mp3');
+            audio4.play();
+            break;
+
+        case "j":
+            let audio5 = new Audio('sounds/tom-2.mp3');
+            audio5.play();
+            break;
+
+        case "k":
+            let audio6 = new Audio('sounds/tom-3.mp3');
+            audio6.play();
+            break;
+
+        case "l":
+            let audio7 = new Audio('sounds/crash.mp3');
+            audio7.play();
+            break;
+            
+        default: console.log(button.innerHTML);
+            
+    }
+
+}
+
+function addAnimation(pressedkey){
+
+    let activebutton = document.querySelector("."+pressedkey);
+
+    activebutton.classList.add("pressed");
+
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    }, 100);
+
+}
